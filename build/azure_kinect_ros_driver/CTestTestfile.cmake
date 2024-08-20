87b7698de32027beb694ff9d72dd510d5ef3bf63
spot <shenlirobot@gmail.com>
2024-08-20 15:28:55 -0400

    publish tf from joint

--- build/azure_kinect_ros_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/spot/dev/ros2_ws/src/Azure_Kinect_ROS_Driver
-# Build directory: /home/spot/dev/ros2_ws/src/Azure_Kinect_ROS_Driver/build/azure_kinect_ros_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
